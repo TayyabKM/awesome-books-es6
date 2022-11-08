1dfe52d019b02da7b56cb1229544cb954d163059
Tayyab Kamboh <tayyabirfan05@gmail.com>
2022-11-08 21:01:59 +0500

    FIx linter errors

--- modules/store.js
+++ modules/store.js
@@ -7,19 +7,19 @@ export default class Store {
       } else {
         books = JSON.parse(localStorage.getItem('books'));
       }
-  
+
       return books;
     }
-  
+
     static addBook = (book) => {
       const books = Store.getBooks();
       books.push(book);
       localStorage.setItem('books', JSON.stringify(books));
     }
-  
+
     static removeBook = (index) => {
       const books = Store.getBooks();
       books.splice(index, 1);
       localStorage.setItem('books', JSON.stringify(books));
     }
-  }
+}
